27bbec5ab85949316a3bfe5fbf0c260a9f72bde6
zhangsan <123@qq.com>
2020-12-31 09:17:08 +0800

    dev分支测试类添加主函数

---
+++ src/test/java/com/baizhi/ideaTest.java
@@ -0,0 +1,20 @@
+package com.baizhi;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author ideaTest
+ * @time 2020/12/31-9:05
+ */
+public class ideaTest {
+    @Test
+    void contextLoads() {
+        int a = 20;
+        int b = 30;
+    }
+
+    public static void main(String[] args) {
+        int a = 20;
+        System.out.println(a);
+    }
+}
