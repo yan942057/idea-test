377da3f840771842ddce7a23fddd36d355b9b6d3
zhangsan <123@qq.com>
2021-01-03 14:52:33 +0800

    测试类修改代码2

--- src/test/java/com/baizhi/ideaTest.java
+++ src/test/java/com/baizhi/ideaTest.java
@@ -16,6 +16,11 @@ public class ideaTest {
     public static void main(String[] args) {
         int a = 20;
         System.out.println(a);
+        System.out.println("1111111");
+        System.out.println("1111111");
+        System.out.println("1111111");
+        System.out.println("1111111");
+        System.out.println("1111111");
     }
 
     public void tests() {
