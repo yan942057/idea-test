5be6f94f7aff1807f9b1f1aef19d0d33ee887fd9
zhangsan <123@qq.com>
2021-01-03 15:55:39 +0800

    用户添加查所有功能

--- src/main/java/com/baizhi/dao/UserDao.java
+++ src/main/java/com/baizhi/dao/UserDao.java
@@ -2,6 +2,10 @@ package com.baizhi.dao;
 
 import com.baizhi.entity.User;
 
+import java.util.List;
+
 public interface UserDao {
     User queryByUserId(String id);
+
+    List<User> queryByUser();
 }
