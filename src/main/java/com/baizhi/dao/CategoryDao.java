b67e3079dbbc92ecb627366c86e8bb14aadda921
zhangsan <123@qq.com>
2021-01-03 15:57:05 +0800

    CategoryDao添加查所有功能

--- src/main/java/com/baizhi/dao/CategoryDao.java
+++ src/main/java/com/baizhi/dao/CategoryDao.java
@@ -1,10 +1,13 @@
 package com.baizhi.dao;
 
 import com.baizhi.entity.Category;
+import com.baizhi.entity.User;
 
 import java.util.List;
 
 public interface CategoryDao {
 
     List<Category> queryByCategory();
+
+    Category queryById(String id);
 }
